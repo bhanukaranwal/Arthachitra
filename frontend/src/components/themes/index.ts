f45220b561b0d692fe47e38fd159032c33f92dab
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-09-07 04:14:45 +0530

    Create index.ts

---
+++ frontend/src/components/themes/index.ts
@@ -0,0 +1,4 @@
+export { ThemeProvider, useTheme } from './ThemeProvider';
+export { ThemeSwitcher, ThemePreview } from './ThemeSwitcher';
+export type { Theme } from './ThemeProvider';
+export * from './useTheme';
